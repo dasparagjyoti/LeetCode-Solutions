87b2e1efd0584c4e43d798ec278f21a95e806b4e
P.J.DAS@21 <paragjyotidas21@gmail.com>
2022-08-30 20:35:49 +0530

    Time: 11 ms (93.39%), Space: 45.4 MB (86.40%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,24 +1,26 @@
 import java.util.*;
-public class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int n: nums){
-            map.put(n, map.getOrDefault(n,0)+1);
-        }
-           
-        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = 
-                         new PriorityQueue<>((a,b)->(b.getValue()-a.getValue()));
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            maxHeap.add(entry);
-        }
-        
-        List<Integer> res = new ArrayList<>();
-        while(res.size()<k){
-            Map.Entry<Integer, Integer> entry = maxHeap.poll();
-            res.add(entry.getKey());
-        }
-        return res.stream().mapToInt(i->i).toArray();
-    }
+public class Solution{
+public int[] topKFrequent(int[] nums, int k) {
+	
+	List<Integer>[] freqSorted = new List[nums.length +1];
+	Map<Integer, Integer> frequencyMap = new HashMap();
+	List<Integer> res = new ArrayList();
+    for(int n: nums)
+		frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+
+ 
+   for(int key: frequencyMap.keySet()){
+	   if(freqSorted[frequencyMap.get(key)] == null)
+		   freqSorted[frequencyMap.get(key)] = new ArrayList();
+	   freqSorted[frequencyMap.get(key)].add(key);
+   }
+
+  
+   for(int i = freqSorted.length - 1; i >= 0 && res.size() < k; i--)
+	   if(freqSorted[i] != null){
+			res.addAll(freqSorted[i]);
+  }
+
+  return res.stream().mapToInt(i->i).toArray();
 }
-
-
+}
