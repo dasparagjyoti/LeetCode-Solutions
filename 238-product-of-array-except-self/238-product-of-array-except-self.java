ef2684668903cefe095166b5a019ecad0fcabd6f
P.J.DAS@21 <paragjyotidas21@gmail.com>
2022-09-01 01:07:22 +0530

    Time: 2 ms (89.42%), Space: 58.2 MB (23.93%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,5 +1,9 @@
 public class Solution {
 public int[] productExceptSelf(int[] nums) {
+    if(nums == null || nums.length == 0){
+        return new int[0];
+    }
+    
     int[] res = new int[nums.length];
     res[0] = 1;
     for (int i = 1; i < nums.length; i++) {
